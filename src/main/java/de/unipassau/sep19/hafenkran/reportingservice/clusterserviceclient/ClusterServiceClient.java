627c02413fe49c381f10a77ebb81cf4df278cdf0
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-12-14 18:30:47 +0100

    EPDS02-174 replace WebClient with RestTemplate

--- src/main/java/de/unipassau/sep19/hafenkran/reportingservice/clusterserviceclient/ClusterServiceClient.java
+++ src/main/java/de/unipassau/sep19/hafenkran/reportingservice/clusterserviceclient/ClusterServiceClient.java
@@ -1,22 +1,31 @@
 package de.unipassau.sep19.hafenkran.reportingservice.clusterserviceclient;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class ClusterServiceClient {
 
-    private final WebClient client;
+    @Value("${clusterservice.path}")
+    private String basePath;
 
-    ClusterServiceClient() {
-        client = WebClient.create("http://localhost:8082");
+    public <T> T get(String path, Class<T> responseType) {
+        RestTemplate rt = new RestTemplate();
+        String targetPath = basePath + path;
+        ResponseEntity<T> response = rt.exchange(basePath + path, HttpMethod.GET, null, responseType);
+
+        if (!HttpStatus.Series.valueOf(response.getStatusCode()).equals(HttpStatus.Series.SUCCESSFUL)) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    String.format("Could not retrieve data from %s. Reason: %s %s", targetPath,
+                            response.getStatusCodeValue(), response.getBody()));
+        }
+
+        return response.getBody();
     }
 
-    public Mono<String> get(String path) {
-        client.get()
-                .uri(path)
-                .exchange()
-                .then(response -> response.bodyToMono());
-    }
 }
--- src/main/java/de/unipassau/sep19/hafenkran/reportingservice/service/impl/ResultServiceImpl.java
+++ src/main/java/de/unipassau/sep19/hafenkran/reportingservice/service/impl/ResultServiceImpl.java
@@ -1,13 +1,21 @@
 package de.unipassau.sep19.hafenkran.reportingservice.service.impl;
 
+import de.unipassau.sep19.hafenkran.reportingservice.clusterserviceclient.ClusterServiceClient;
 import de.unipassau.sep19.hafenkran.reportingservice.model.Result;
 import de.unipassau.sep19.hafenkran.reportingservice.service.ResultService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64InputStream;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.*;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,7 +27,22 @@ import java.util.UUID;
 @Slf4j
 public class ResultServiceImpl implements ResultService {
 
-    private List<Result> retrieveRemoteResultsForExecution(UUID executionId){
-        WebClient.create().
+    @NonNull
+    private ClusterServiceClient csClient;
+
+    @Value()
+
+    private List<Result> storeRemoteResultsOfExecutionToFile(UUID executionId) {
+        String response = csClient.get(String.format("/executions/%s/results", executionId), String.class);
+        InputStream is = new Base64InputStream(new ByteArrayInputStream(response.getBytes()));
+        ArchiveInputStream archive = new TarArchiveInputStream(is);
+
+        File targetFile = new File("src/main/resources/targetFile.tmp");
+
+        try {
+            FileUtils.copyInputStreamToFile(archive, targetFile);
+        } catch (IOException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not resolve the results file to the file system", e);
+        }
     }
 }
