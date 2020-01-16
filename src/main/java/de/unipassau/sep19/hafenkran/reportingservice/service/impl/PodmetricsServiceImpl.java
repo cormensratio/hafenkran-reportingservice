006e34eae921df160982ed0bad6d29074c74e527
Martin Loos <martinjloos@mail.de>
2020-01-16 01:18:09 +0100

    EPDS02-50 EPDS02-211 added PodmetricsServiceImpl

---
+++ src/main/java/de/unipassau/sep19/hafenkran/reportingservice/service/impl/PodmetricsServiceImpl.java
@@ -0,0 +1,50 @@
+package de.unipassau.sep19.hafenkran.reportingservice.service.impl;
+
+import de.unipassau.sep19.hafenkran.reportingservice.dto.MetricsDTO;
+import de.unipassau.sep19.hafenkran.reportingservice.dto.MetricsDTOList;
+import de.unipassau.sep19.hafenkran.reportingservice.model.Podmetrics;
+import de.unipassau.sep19.hafenkran.reportingservice.repository.PodmetricsRepository;
+import de.unipassau.sep19.hafenkran.reportingservice.service.PodmetricsService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+/**
+ * {@inheritDoc}
+ */
+@Service
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
+public class PodmetricsServiceImpl implements PodmetricsService {
+
+    @NonNull
+    private final PodmetricsRepository podmetricsRepository;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public MetricsDTOList retrieveMetricsDTOListByExecutionId(@NonNull UUID executionId) {
+        List<Podmetrics> metricsList = retrievePodmetricsListByExecutionId(executionId);
+        List<MetricsDTO> metricsDTOList = metricsList.stream().map(r -> new MetricsDTO(
+                r.getCpu(),
+                r.getMemory(),
+                r.getTimestamp())).collect(Collectors.toList());
+        return new MetricsDTOList(metricsDTOList, executionId);
+    }
+
+    @Override
+    public void savePodmetrics(@NonNull Podmetrics podmetrics) {
+        podmetricsRepository.save(podmetrics);
+    }
+
+    private List<Podmetrics> retrievePodmetricsListByExecutionId(@NonNull UUID executionId) {
+        return podmetricsRepository.findAllByExecutionId(executionId);
+    }
+}
