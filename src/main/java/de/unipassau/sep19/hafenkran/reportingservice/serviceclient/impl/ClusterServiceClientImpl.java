aaeac1e9d22bf0009cc069e14b3111989278d587
Martin Loos <martinjloos@mail.de>
2020-01-28 14:58:08 +0100

    EPDS02-211 retrieveallpodmetrics

--- src/main/java/de/unipassau/sep19/hafenkran/reportingservice/serviceclient/impl/ClusterServiceClientImpl.java
+++ src/main/java/de/unipassau/sep19/hafenkran/reportingservice/serviceclient/impl/ClusterServiceClientImpl.java
@@ -1,11 +1,14 @@
 package de.unipassau.sep19.hafenkran.reportingservice.serviceclient.impl;
 
+import de.unipassau.sep19.hafenkran.reportingservice.dto.CsPodmetricsDTO;
+import de.unipassau.sep19.hafenkran.reportingservice.service.PodmetricsService;
 import de.unipassau.sep19.hafenkran.reportingservice.serviceclient.ClusterServiceClient;
 import de.unipassau.sep19.hafenkran.reportingservice.serviceclient.ServiceClient;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -16,6 +19,11 @@ public class ClusterServiceClientImpl implements ClusterServiceClient {
 
     private final ServiceClient serviceClient;
 
+    private final PodmetricsService podmetricsService;
+
+    @Value("${cluster-service-uri}")
+    private String clusterServiceURI;
+
     @Value("${service-user.secret}")
     private String serviceSecret;
 
@@ -25,5 +33,13 @@ public class ClusterServiceClientImpl implements ClusterServiceClient {
     public String retrieveResultsForExecutionId(@NonNull UUID executionId) {
         return serviceClient.get(String.format("/executions/%s/results?secret=" + serviceSecret, executionId),
                 String.class, null);
+    }
+
+    @Scheduled(fixedRateString = "#{${metrics.frequency} * 1000}",
+            initialDelayString = "#{${metrics.initial-delay} * 1000}")
+    public void retrieveAllPodmetrics() {
+        CsPodmetricsDTO[] podmetricsDTOs = serviceClient.get(clusterServiceURI + "/metrics/all",
+                CsPodmetricsDTO[].class, null);
+        podmetricsService.savePodmetrics(podmetricsDTOs);
     }
 }
