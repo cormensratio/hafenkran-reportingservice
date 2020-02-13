3698e2aafb8f031095332def8fd872bd10940879
Simon Lichtnecker <cormensratio@posteo.de>
2020-02-13 15:34:07 +0100

    Deleted unnecessary test file

--- src/test/java/de/unipassau/sep19/hafenkran/reportingservice/service/impl/ResultServiceImplTest.java
+++
@@ -1,8 +0,0 @@
-package de.unipassau.sep19.hafenkran.reportingservice.service.impl;
-
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.class)
-public class ResultServiceImplTest {
-}
